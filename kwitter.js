11113f69271d8b183dafc3c06a6bda7be3f73303
Monaliza Loren <83433815+monalizaloren@users.noreply.github.com>
2023-05-23 07:37:52 -0300

    Update kwitter.js

--- kwitter.js
+++ kwitter.js
@@ -1,11 +1,1 @@
 
-function addUser() {
-
-  userName = document.getElementById("userName").value;
-// Armazenado local do navegador
-// Variavel que guarda o nome do usuário
-  ????.setItem("userName", ???);
-  //Código que muda de tela
-    ???? = "kwitterRoom.html";
-}
-
